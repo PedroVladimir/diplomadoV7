a0d18d0126129eed933c588c52b952ad3b73215d
PedroVladimir <pvfloresrios@gmail.com>
2025-06-27 22:07:11 -0400

    fix env

--- src/config/env.js
+++ src/config/env.js
@@ -18,6 +18,7 @@ const config = {
     BCRYPT_SALT_ROUNDS: +process.env.BCRYPT_SALT_ROUNDS,
     JWT_SECRET: process.env.JWT_SECRET,
     JWT_EXPIRES_SECONDS: process.env.JWT_EXPIRES_SECONDS,
+    DB_USE_SSL: process.env.DB_USE_SSL,
 }; 
 
 export default config;
